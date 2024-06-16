06ed83341f907716387db39efa8979ac20588dd2
isamu <isamu@to-kyo.to>
2024-06-17 05:22:20 +0900

    litegraph

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,29 +10,46 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "vue";
 
-import { LGraph, LGraphCanvas, LiteGraph } from "litegraph.js";
-import "litegraph.js/css/litegraph.css"
+import { LGraph, LGraphCanvas, LiteGraph, LGraphNode } from "litegraph.js";
+import "litegraph.js/css/litegraph.css";
+
+import { BasicSumAgent, StringInputNode, TextInputAgentNode, OpenAIAgentNode, StringTemplateAgentNode, PropertyFilterAgentNode } from "./nodes";
 
 export default defineComponent({
   name: "HomePage",
   components: {},
   setup() {
     const canvasRef = ref();
-    onMounted(() => {
-      const graph = new LGraph();
 
+    // init menu
+    LiteGraph.registered_node_types = {};
+    LiteGraph.searchbox_extras = {};
+
+    LiteGraph.registerNodeType("basic/sum", BasicSumAgent);
+    LiteGraph.registerNodeType("basic/stringInput", StringInputNode);
+    
+    LiteGraph.registerNodeType("graphai/TextInputAgent", TextInputAgentNode);
+    LiteGraph.registerNodeType("graphai/OpenAIAgent", OpenAIAgentNode);
+    
+    LiteGraph.registerNodeType("graphai/StringTemplateAgent", StringTemplateAgentNode);
+    LiteGraph.registerNodeType("graphai/PropertyFilterAgent", PropertyFilterAgentNode);
+    
+    onMounted(() => {
+
+      const graph = new LGraph();
       const canvas = new LGraphCanvas(canvasRef.value, graph);
 
-      const node_const = LiteGraph.createNode("basic/const");
+      const node_const = LiteGraph.createNode("basic/sum");
       node_const.pos = [200, 200];
       graph.add(node_const);
-      node_const.setValue(4.5);
+      // node_const.setValue(4.5);
 
-      const node_watch = LiteGraph.createNode("basic/watch");
+      const node_watch = LiteGraph.createNode("basic/sum");
       node_watch.pos = [700, 200];
       graph.add(node_watch);
 
       node_const.connect(0, node_watch, 0);
+
       graph.start();
     });
     return {
---
+++ src/views/nodes.ts
@@ -0,0 +1,76 @@
+import { LGraphNode } from "litegraph.js";
+
+export class BasicSumAgent extends LGraphNode {
+  constructor() {
+    super("Sum");
+    this.addInput("A", "number");
+    this.addInput("B", "number");
+    this.addOutput("A+B", "number");
+    this.properties = { precision: 1 };
+  }
+
+  onExecute() {
+    let A = this.getInputData(0);
+    if (A === undefined) A = 0;
+    let B = this.getInputData(1);
+    if (B === undefined) B = 0;
+    this.setOutputData(0, A + B);
+  }
+}
+
+export class StringInputNode extends LGraphNode {
+  constructor() {
+    super("String Input");
+    this.addOutput("Output", "string");
+    this.properties = { text: "" };
+  }
+  onDrawBackground(ctx) {
+    if (this.flags.collapsed) {
+      return;
+    }
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, this.size[0], this.size[1]);
+    ctx.fillStyle = "#FFF";
+    ctx.font = "14px Arial";
+    ctx.fillText(this.properties.text, 10, 30);
+  }
+}
+
+export class TextInputAgentNode extends LGraphNode {
+  constructor() {
+    super("TextInput");
+    this.properties = { message: "?:" };
+    this.addOutput("", "string");
+  }
+}
+
+export class OpenAIAgentNode extends LGraphNode {
+  constructor() {
+    super("OpenAI");
+    this.addInput("In", "string");
+    this.addOutput(".choices.$0.message.content", "string");
+  }
+}
+
+export class StringTemplateAgentNode extends LGraphNode {
+  constructor() {
+    super("StringTemplate");
+    this.properties = { template: "${0}" };
+    this.addInput("${0}", "string");
+    this.addOutput("Output", "string");
+  }
+
+  onPropertyChanged(name, value) {
+    this.properties[name] = value;
+  }
+}
+
+export class PropertyFilterAgentNode extends LGraphNode {
+  constructor() {
+    super("PropertyFilter");
+    this.addInput("In", "string");
+    this.addOutput("", "string");
+  }
+
+  onExecute = function () {};
+}
