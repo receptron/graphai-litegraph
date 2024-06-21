07fd0ecbeaa520e7157ebbec9599ea2eaf1d0912
isamu <isamu@to-kyo.to>
2024-06-22 06:50:38 +0900

    graph

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,22 @@
       <!-- Use Tailwind CSS h-40 (=10rem=160px) instead of .logo. -->
       <canvas id="mycanvas" width="1024" height="720" style="border: 1px solid" ref="canvasRef"></canvas>
     </div>
-    <button @click="save" class="border-2 border-blue-200">Save</button>
-    <button @click="load" class="border-2 border-blue-200">Load</button>
-    <button @click="reset" class="border-2 border-blue-200">Reset</button>
-    <button @click="download" class="border-2 border-blue-200">Download</button>
-    <button @click="runGraph" class="border-2 border-blue-200">Run</button>
+    <div>
+      <button @click="save" class="border-2 border-blue-200">Save</button>
+      <button @click="load" class="border-2 border-blue-200">Load</button>
+      <button @click="reset" class="border-2 border-blue-200">Reset</button>
+      <button @click="download" class="border-2 border-blue-200">Download</button>
+      <button @click="runGraph" class="border-2 border-blue-200">Run</button>
+    </div>
+    <div>
+      {{ streamingData }}
+      {{ graphResult }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, watch } from "vue";
 
 import { LGraph, LGraphCanvas } from "litegraph.js";
 import "litegraph.js/css/litegraph.css";
@@ -21,11 +27,14 @@ import "litegraph.js/css/litegraph.css";
 import { liteGraph2GraphData } from "../utils/toGraphAi";
 import { LiteGraph, setAgentToLiteGraph } from "../utils/setAgentToLiteGraph";
 
-import { GraphAI } from "graphai";
+// import { GraphAI } from "graphai";
 
 import * as vanillaAgents from "@graphai/vanilla";
-import { agentlist } from "../utils/agentlist";
+import * as sleeperAgents from "@graphai/sleeper_agents";
+import * as dataAgents from "@graphai/data_agents"
+// import { agentlist } from "../utils/agentlist";
 import { defaultData } from "../utils/defaultData";
+import { useGraph } from "../utils/graph";
 
 export default defineComponent({
   name: "HomePage",
@@ -34,60 +43,62 @@ export default defineComponent({
     const canvasRef = ref();
 
     let lite2graph = {};
-
     let liteGraph: LGraph | undefined = undefined;
+    const webAgents = { ...vanillaAgents,  ...sleeperAgents, ...dataAgents };
+    const {
+      getGraph,
+      streamingData,
+      graphResult,
+      serverAgentsInfoDictionary,
+    } = useGraph(["http://localhost:8085/agents"], webAgents);
 
-    onMounted(() => {
-      const agents = agentlist.agents.reduce((tmp: any, agent) => {
-        tmp[agent.agentId] = agent;
-        return tmp;
-      }, {});
-      lite2graph = setAgentToLiteGraph(agents);
-      liteGraph = new LiteGraph.LGraph();
-
-      if (!load()) {
-        liteGraph.configure(defaultData);
+    watch([canvasRef, serverAgentsInfoDictionary], (v) => {
+      if (v[0] && v[1] && Object.keys(v[1]).length > 0) {
+        const agents = [...Object.values(serverAgentsInfoDictionary.value), ...Object.values(webAgents)].reduce((tmp: any, agent) => {
+          tmp[agent.agentId ?? agent.name] = agent;
+          return tmp;
+        }, {});
+        lite2graph = setAgentToLiteGraph(agents);
+        liteGraph = new LiteGraph.LGraph();
+        
+        new LGraphCanvas(canvasRef.value, liteGraph);
+        liteGraph.start();
+        if (!load()) {
+          liteGraph.configure(defaultData);
+        }
       }
-      new LGraphCanvas(canvasRef.value, liteGraph);
-
-      liteGraph.start();
     });
-
+    
     const download = () => {
-      if (liteGraph) {
-        const data = liteGraph.serialize();
-        console.log(JSON.stringify(data));
-        const graphData = liteGraph2GraphData(data, lite2graph as any);
-        console.log(graphData);
-        console.log(JSON.stringify(graphData, undefined, 2));
-      }
+      const data = liteGraph.serialize();
+      console.log(JSON.stringify(data));
+      const graphData = liteGraph2GraphData(data, lite2graph as any);
+      console.log(graphData);
+      console.log(JSON.stringify(graphData, undefined, 2));
     };
     const localStorageKey = "graphai-litegraph-data";
 
     const save = () => {
-      if (liteGraph) {
-        const data = liteGraph.serialize();
-        localStorage.setItem(localStorageKey, JSON.stringify(data));
-      }
+      const data = liteGraph.serialize();
+      localStorage.setItem(localStorageKey, JSON.stringify(data));
     };
     const load = () => {
       const graphString = localStorage.getItem(localStorageKey);
-      if (graphString && liteGraph) {
+      if (graphString) {
         const data = JSON.parse(graphString);
         liteGraph.configure(data);
       }
       return !!graphString;
     };
     const reset = () => {
-      if (liteGraph) {
-        liteGraph.configure(defaultData);
-      }
+      liteGraph.configure(defaultData);
     };
     const runGraph = async () => {
       const data = liteGraph.serialize();
       const graphData = liteGraph2GraphData(data, lite2graph as any);
-      const graphAI = new GraphAI(graphData,  vanillaAgents);
+      const graphAI = getGraph(graphData);
       const res = await graphAI.run(true);
+      graphResult.value = res;
       console.log(res);
     };
     return {
@@ -97,6 +108,9 @@ export default defineComponent({
       load,
       reset,
       runGraph,
+
+      streamingData,
+      graphResult,
     };
   },
 });
